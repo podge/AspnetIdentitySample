b4d6c87b43118a59be47f6757925697e7b351fab
Podge <lavinp@yahoo.com>
2015-04-27 09:42:31 +0100

    PDF copy working

--- AspnetIdentitySample/Controllers/CertGeneratorController.cs
+++ AspnetIdentitySample/Controllers/CertGeneratorController.cs
@@ -11,13 +11,59 @@ namespace AspnetIdentitySample.Controllers
 {
     public class CertGeneratorController : Controller
     {
+        string originalFile = "c:\\eupp\\pet_cert.pdf";
+        string copyOfOriginal = "c:\\eupp\\Copy.pdf";
+
         // GET: CertGenerator
         public ActionResult Index(int? id)
         {
+            CreatePDFByCopy();
+            return View();
+        }
+
+        private void CreatePDFByCopy()
+        {
+            using (Document document = new Document())
+            {
+                using (PdfSmartCopy copy = new PdfSmartCopy(document, new FileStream(copyOfOriginal, FileMode.Create)))
+                {
+                    document.Open();
+                    for (int i = 0; i < 6; ++i)
+                    {
+                        PdfReader reader = new PdfReader(AddDataSheets("Some Text" + i.ToString()));
+                        copy.AddPage(copy.GetImportedPage(reader, i+1));
+                    }
+                }
+            }
+        }
+
+        public byte[] AddDataSheets(string _data)
+        {
+            string pdfTemplatePath = originalFile;
+            PdfReader reader = new PdfReader(pdfTemplatePath);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (PdfStamper stamper = new PdfStamper(reader, ms))
+                {
+                    AcroFields form = stamper.AcroFields;
+                    var fieldKeys = form.Fields.Keys;
+                    foreach (string fieldKey in fieldKeys)
+                    {
+                        //Change some data
+                        if (fieldKey.Contains("Address"))
+                        {
+                            form.SetField(fieldKey, _data);
+                        }
+                    }
+                    stamper.FormFlattening = true;
+                }
+                return ms.ToArray();
+            }
+        }
+
+        public void first(){
             //FileStream fs = new FileStream("c:\\eupp\\Chapter1_Example1.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
 
-            string originalFile = "c:\\eupp\\pet_cert.pdf";
-            string copyOfOriginal = "c:\\eupp\\Copy.pdf";
             using (FileStream fs = new FileStream(originalFile, FileMode.Create, FileAccess.Write, FileShare.None))
             using (Document doc = new Document(PageSize.LETTER))
             using (PdfWriter writer = PdfWriter.GetInstance(doc, fs))
@@ -29,8 +75,6 @@ namespace AspnetIdentitySample.Controllers
             PdfReader reader = new PdfReader(originalFile);
             using (FileStream fs = new FileStream(copyOfOriginal, FileMode.Create, FileAccess.Write, FileShare.None))
             using (PdfStamper stamper = new PdfStamper(reader, fs)) { }
-
-            return View();
         }
     }
 }
