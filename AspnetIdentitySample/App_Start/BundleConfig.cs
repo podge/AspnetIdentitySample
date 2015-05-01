b1374aa5a3758490d53eababd592539be387ae20
Podge <lavinp@yahoo.com>
2015-05-01 14:04:18 +0100

    DatePicker JQuery and other changes on Edit Pet page

--- AspnetIdentitySample/App_Start/BundleConfig.cs
+++ AspnetIdentitySample/App_Start/BundleConfig.cs
@@ -41,6 +41,15 @@ namespace AspnetIdentitySample
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js",
+                        "~/Scripts/jquery-ui.unobtrusive-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.theme.css")); 
         }
     }
 }
